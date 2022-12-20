264bf74b45ec46dd849d96ebd827c6b5780cc364
rubhain <bugrahantufekcioglu@gmail.com>
2022-12-20 13:48:56 +0300

    Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Notes App</h1>
+      <Search />
       <NoteForm />
+      <NoteList />
     </div>
   );
 }
